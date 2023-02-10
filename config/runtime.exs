b2c17a3c51efed3c3844c98380a072662a097dce
shu_0 <shu.kwn0257b@gmail.com>
2023-02-11 03:59:05 +0900

    error_removed

--- config/runtime.exs
+++ config/runtime.exs
@@ -21,18 +21,18 @@ import Config
 # end
 
 if config_env() == :prod do
-  database_url =
-    System.get_env("DATABASE_URL") ||
-      raise """
-      environment variable DATABASE_URL is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
+  #database_url =
+  #  System.get_env("DATABASE_URL") ||
+  #    raise """
+  #    environment variable DATABASE_URL is missing.
+  #    For example: ecto://USER:PASS@HOST/DATABASE
+  #    """
 
   maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
 
   config :sample_daisyui, SampleDaisyui.Repo,
     # ssl: true,
-    url: database_url,
+    #url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
 
